13882b420f1a5cead2e4c19153920a8d057f0754
aali-bright <ayman@brightlive.com>
2022-02-14 21:12:40 -0800

    fix remove attribute

--- src/util.js
+++ src/util.js
@@ -240,12 +240,12 @@ async function commit(page, details, row) {
           await page.waitForSelector(detail.selector);
           await page.focus(detail.selector);
           await page.type(detail.selector, "");
-          const removeReadOnlyAttribute = `document.querySelector('${detail.select}').removeAttribute('readonly')`
-          const removeDisabledAttribute = `document.querySelector('${detail.select}').removeAttribute('disabled')`
           await page.evaluate(removeDisabledAttribute)
           await page.evaluate(removeReadOnlyAttribute)
           await page.evaluate((element) => {
             const field = document.querySelector(element.selector);
+            field.removeAttribute('readonly');
+            field.removeAttribute('disabled');
             if (field) {
               field.value = "";
               field.setAttribute("value", "");
