902322b954e320916d8380b539cb7e7db447ad5f
aliayman <aliayman@alumni.harvard.edu>
2022-05-03 20:14:11 -0700

    vision

--- src/vsn.js
+++ src/vsn.js
@@ -19,7 +19,7 @@ async function send(sendData) {
   data = sendData;
   const inputFolder = path.join(__dirname, "..", "scan", "input");
   if (!fs.existsSync(inputFolder)) {
-    fs.mkdirSync(inputFolder, { recursive: true });
+    return fs.mkdirSync(inputFolder, { recursive: true });
   }
 
   const outputFolder = path.join(__dirname, "..", "scan", "input", "done");
@@ -38,44 +38,117 @@ async function send(sendData) {
 
   for (let i = 0; i < data?.travellers?.length; i++) {
     const traveler = data.travellers[i];
-    const localImagePath = path.join(
-      inputFolder,
-      traveler.nationality.name + "-" + traveler.passportNumber.toString()
+    // if passport seen by vision api then continue with eagle detection
+    // else perform vision api label annotation
+    const documentJsonPath = path.join(
+      __dirname,
+      "..",
+      "scan",
+      "input",
+      "done",
+      `${traveler.nationality.name}-${traveler.passportNumber.toString()}.json`
     );
-    await util.downloadImage(traveler.images.passport, localImagePath);
-     client.textDetection(localImagePath).then((results) => {
-      const labels = results[0].textAnnotations;
-      labels.forEach((label) => {
-        // find MRZ and write it to CODELINE.txt
-        // find egyptianId number and write it to nn-ID.txt
-        // find issue date and write it to nn-ISSUEDT.txt
-        // find profession and write it to nn-profession.txt
-        // find issue place/ office and write it to nn-issueplace.txt
-        // find arabic name and write it to nn-arabic-name.txt
-        // find address and write it to nn-address.txt
-        // etc ..
-        // tODO:Inquire about output path
-      });
-      const outputFile = path.join(
-        outputFolder,
-        `${
-          traveler.nationality.name + "-" + traveler.passportNumber.toString()
-        }.txt`
-      );
-      console.log('write to => ', outputFile);
-      fs.writeFileSync(
-        outputFile,
-        JSON.stringify(results, null, 2)
-      );
-    })
-    .catch((err) => {
-      console.error("ERROR:", err);
-    });
-
-
-
+    const document_seen = fs.existsSync(documentJsonPath);
+    if (document_seen) {
+      const rawData = fs.readFileSync(documentJsonPath, "utf-8");
+      const annotations = JSON.parse(rawData);
+      const eagleExtract = runEagleExtract(annotations);
+      continue;
+    }
+   //  await seeTravelerDocuments(inputFolder, traveler, client, documentJsonPath);
     // TODO: save the parsed passport data to firebase or create another data.json file that can be imported to firebase
   }
 }
 
 module.exports = { send };
+
+async function seeTravelerDocuments(
+  inputFolder,
+  traveler,
+  client,
+  done_already_path
+) {
+  const downloadPath = path.join(
+    inputFolder,
+    traveler.nationality.name + "-" + traveler.passportNumber.toString()
+  );
+  const passportUrl = traveler.images.passport;
+  await util.downloadImage(passportUrl, downloadPath);
+  client
+    .textDetection(downloadPath)
+    .then((results) => {
+      console.log("write to => ", done_already_path);
+      fs.writeFileSync(done_already_path, JSON.stringify(results, null, 2));
+    })
+    .catch((err) => {
+      console.error("ERROR:", err);
+    });
+}
+
+let eagleVisionResult = {};
+function runEagleExtract(annotations) {
+  eagleVisionResult = {};
+  const labels = annotations[0].textAnnotations;
+  const interestingLabels = labels.filter((label) => {
+    return label.description.length > 0;
+  });
+  const extract = {
+    ...getMRZ(interestingLabels),
+    ...getDates(interestingLabels),
+    ...getProfession(interestingLabels),
+    ...getEgyptianId(interestingLabels),
+    ...getIssuePlace(interestingLabels),
+    ...getArabicName(interestingLabels),
+    ...getAddress(interestingLabels),
+  };
+  createDeviceFiles(extract);
+  return extract;
+}
+
+function createDeviceFiles(extract) {
+  // 3M files are codeline, IMAGEVIS, PHOTO
+  // fs.writeFileSync("codeline.txt", extract.mrz);
+}
+
+function getMRZ(labels) {
+  const mrz = labels.filter((label) => {
+    return /^[A-Z0-9<]{22,88}$/.test(label.description);
+  });
+
+  if (mrz) {
+    eagleVisionResult.mrz = mrz.map((label) => {
+      return label.description;
+    });
+  }
+}
+function getDates(labels) {
+  // Better regex to match date is needed
+  // const dates = labels.map((label) => {
+  //   return label.description.match(/[0-9]{2,4}.?[0-9]{2}.?[0-9]{2,4}/)?.[0];
+  // })?.filter(x => x);
+  // let earliest ;
+
+  // for (const oneDate of dates) {
+  //   const aDate = moment(oneDate);
+  //   if (!aDate.isValid()) {
+  //     continue;
+  //   }
+
+  //   if (!earliest) {
+  //     earliest = oneDate;
+  //   } else {
+  //     if (moment(oneDate).isBefore(earliest)) {
+  //       earliest = oneDate;
+  //     }
+  //   }
+  // }
+  // if (earliest) {
+  //   eagleVisionResult.dateOfBirth = moment(earliest).format("DD/MM/YYYY");
+  // }
+}
+
+function getProfession(labels) {}
+function getEgyptianId(labels) {}
+function getIssuePlace(labels) {}
+function getArabicName(labels) {}
+function getAddress(labels) {}
