0a9e47b7c2d226fc2b81ed3eee035837f9a69c86
hajonsoft <aliayman@alumni.harvard.edu>
2022-11-28 21:21:07 -0800

    initial files

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const { send: sendVst } = require("./src/vst");
 const { send: sendEnj } = require("./src/enj");
 const { send: sendTwf } = require("./src/twf");
 const { send: sendHsf } = require("./src/hsf");
+const { send: sendHsfi } = require("./src/hsfi");
 const { send: sendSbr } = require("./src/sbr");
 const { send: sendMtf } = require("./src/mtf");
 const { send: sendGhb } = require("./src/ghb");
@@ -182,7 +183,10 @@ async function submitToProvider() {
     case "enj":
       return sendEnj(data);
     case "hsf":
-      return sendHsf(data);
+      // TODO: implement hsf individual in kea
+    //   return sendHsf(data);
+    // case "hsfi":
+      return sendHsfi(data);
     case "sbr":
       return sendSbr(data);
     case "mtf":
