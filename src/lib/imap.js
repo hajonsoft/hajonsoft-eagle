659919f1a4be6f0d14d95a3a960bda415f1e0210
HAJonSoft <aliayman@alumni.harvard.edu>
2024-02-08 02:07:35 -0800

    publish (#183)

--- src/lib/imap.js
+++ src/lib/imap.js
@@ -2,7 +2,6 @@ const moment = require("moment");
 const Imap = require("node-imap"),
   inspect = require("util").inspect;
 const fs = require("fs");
-const cheerio = require('cheerio');
 
 const nusukFromEmail = "no_reply@hajj.nusuk.sa";
 const messages = {};
@@ -23,71 +22,91 @@ async function fetchNusukIMAPOTP(recipient, password, subject, callback) {
   imap.once("ready", function () {
     openInbox(function (err, box) {
       if (err) throw err;
-      imap.search(["UNSEEN"], function (err, results) {
-        if (err) throw err;
+      const oneMinuteAgo = new Date();
+      oneMinuteAgo.setMinutes(oneMinuteAgo.getMinutes() - 1);
 
-        if (!results || !results.length) {
-          imap.end();
-          return callback("no-code");
-        }
-        for (let i = 0; i < Math.min(results.length, 5); i++) {
-          var msgSeqNo = results[i];
-          var f = imap.fetch(msgSeqNo, {
-            bodies: ["HEADER.FIELDS (FROM TO SUBJECT DATE)", "TEXT"],
-            struct: true,
-            markSeen: true,
-          });
-        }
+      // Convert the date to a formatted string in the IMAP date format
+      const sinceDate = oneMinuteAgo
+        .toISOString()
+        .slice(0, -5)
+        .replace(/T/g, " ");
 
-        f.on("message", function (msg, seqno) {
-          messages[seqno] = {};
-          msg.on("body", function (stream, info) {
-            var buffer = "";
-            stream.on("data", function (chunk) {
-              buffer += chunk.toString("utf8");
+      const subjectArray = subject.map((s) => ["HEADER", "SUBJECT", s]);
+
+      imap.search(
+        [
+          "UNSEEN",
+          ["OR", ...subjectArray],
+          ["HEADER", "FROM", nusukFromEmail],
+          ["HEADER", "TO", recipient],
+          ["SINCE", sinceDate],
+        ],
+        function (err, results) {
+          if (err) throw err;
+
+          if (!results || !results.length) {
+            imap.end();
+            return callback("no-code");
+          }
+          for (let i = 0; i < Math.min(results.length, 5); i++) {
+            var msgSeqNo = results[i];
+            var f = imap.fetch(msgSeqNo, {
+              bodies: ["TEXT"],
+              struct: true,
+              markSeen: true, 
             });
-            stream.once("end", function () {
-              if (info.which === "TEXT") {
-                const clean = buffer;
-                messages[seqno].body = clean;
-                const englishOtp = messages[seqno].body.match(
-                  /OTP is (\d{6})/
-                )?.[1];
-                if (englishOtp) {
-                  messages[seqno].otp = englishOtp;
-                } else {
-                  const arabicOtp = messages[seqno].body.match(
-                    / هو (\d+)/
-                  )?.[1];
-                  if (arabicOtp) {
-                  } else {
-                    callback("Error: Manual code required");
-                  }
-                }
+          }
+
+          f.on("message", function (msg, seqno) {
+            messages[seqno] = {};
+            let buffer = "";
+            msg.on("body", function (stream, info) {
+              stream.on("data", function (chunk) {
+                buffer += chunk.toString("utf8");
+              });
+            });
+            msg.once("attributes", function (attrs) {
+              messages[seqno].encoding = attrs.struct?.[0]?.encoding;
+            });
+            msg.once("end", function () {
+              switch (messages[seqno].encoding) {
+                case "quoted-printable":
+                  messages[seqno].body = decodeQuotedPrintable(buffer);
+                  break;
+                case "base64":
+                  messages[seqno].body = decodeBase64(buffer);
+                  break;
+                default:
+                  messages[seqno].body = buffer;
+                  break;
+              }
+              const englishOtp =
+                messages[seqno].body.match(/OTP is (\d{6})/)?.[1];
+              if (englishOtp) {
+                messages[seqno].otp = englishOtp;
               } else {
-                messages[seqno].header = Imap.parseHeader(buffer);
+                const arabicOtp =
+                  messages[seqno].body.match(/;&#x648; (\d+)/)?.[1];
+                if (arabicOtp) {
+                  messages[seqno].otp = arabicOtp;
+                }
+              }
+              if (messages[seqno].otp) {
+                messages.codeFound = true;
+                callback(null, messages[seqno].otp);
+                imap.end();
               }
             });
           });
-          msg.once("end", function () {
-            // Apply the filter here and call the callback
-            if (
-              messages[seqno].header.from[0].includes(nusukFromEmail) &&
-              messages[seqno].header.to[0].includes(recipient) &&
-              subject.includes(messages[seqno].header.subject[0])
-            ) {
-              callback(null, messages[seqno].otp);
-              imap.end();
-            }
+          f.once("error", function (err) {
+            callback("Error: " + err);
           });
-        });
-        f.once("error", function (err) {
-          callback("Error: " + err);
-        });
-        f.once("end", function () {
-          imap.end();
-        });
-      });
+          f.once("end", function () {
+            if (!messages.codeFound) callback("no-code");
+            imap.end();
+          });
+        }
+      );
     });
   });
 
@@ -101,6 +120,40 @@ async function fetchNusukIMAPOTP(recipient, password, subject, callback) {
 
   imap.connect();
 }
+
+function decodeBase64(text) {
+  return Buffer.from(text, "base64").toString("utf-8");
+}
+
+function decodeQuotedPrintable(encodedText) {
+  // Replace soft line breaks (encoded as '=') followed by newline characters with empty string
+  let text = encodedText.replace(/=\n/g, "");
+
+  // Replace equal signs followed by two hexadecimal characters with their corresponding ASCII character
+  text = text.replace(/=([0-9A-F]{2})/g, (match, hex) => {
+    return String.fromCharCode(parseInt(hex, 16));
+  });
+
+  // Replace equal signs followed by a single hexadecimal character with their corresponding ASCII character
+  text = text.replace(/=([0-9A-F])/gi, (match, hex) => {
+    return String.fromCharCode(parseInt(hex, 16));
+  });
+
+  // Decode percent-encoded characters
+  // text = decodeURIComponent(text);
+
+  return text;
+}
+
+// test code
+// fetchNusukIMAPOTP(
+//   "HAJJ.ABUOMAR@YOIGOMAIL.COM",
+//   "replace with correct passsword",
+//   ["One Time Password", "رمز سري لمرة واحدة"],
+//   (err, otp) => {
+//     console.log(err, otp);
+//   }
+// );
 
 module.exports = {
   fetchNusukIMAPOTP,
