338a6953ab62ae3b00184de1773eb59a16c145bc
AymanA <aliayman@alumni.harvard.edu>
2024-03-14 04:12:25 -0700

    insurance connection

--- src/lib/insurance.js
+++ src/lib/insurance.js
@@ -1,6 +1,9 @@
 const pdfLib = require("pdf-lib");
 const fs = require("fs");
 const fontkit = require("@pdf-lib/fontkit");
+const path = require("path");
+
+let resultFileName;
 
 const mergeAndSavePDF = async (buffers) => {
   const mergedPdf = await pdfLib.PDFDocument.create();
@@ -18,7 +21,7 @@ const mergeAndSavePDF = async (buffers) => {
 
   const mergedPdfBytes = await mergedPdf.save();
 
-  fs.writeFileSync("./result.pdf", mergedPdfBytes);
+  fs.writeFileSync(resultFileName, mergedPdfBytes);
 };
 
 const createPDF = async (
@@ -30,17 +33,20 @@ const createPDF = async (
   arabicNationality
 ) => {
   try {
-    const templatePdfBytes = fs.readFileSync("../assets/insurance.pdf");
+    const templatePdfBytes = fs.readFileSync(
+      path.join(__dirname, "../assets/insurance.pdf")
+    );
     // Use PDFDocument.load to load the PDF from bytes
     const pdfDoc = await pdfLib.PDFDocument.load(templatePdfBytes);
 
     const helveticaFont = await pdfDoc.embedFont(
       pdfLib.StandardFonts.Helvetica
     );
-
-    const fontBytes = fs.readFileSync(
+    const fontFilePath = path.join(
+      __dirname,
       "../assets/NotoSansArabic-VariableFont_wdth,wght.ttf"
     );
+    const fontBytes = fs.readFileSync(fontFilePath);
     pdfDoc.registerFontkit(fontkit);
 
     const arabicFont = await pdfDoc.embedFont(fontBytes);
@@ -109,7 +115,8 @@ const createPDF = async (
   }
 };
 
-const createInsurance = async (userDetails) => {
+const createInsurance = async (userDetails, fileName) => {
+  resultFileName = fileName;
   try {
     const pdfCreationPromises = userDetails.map((user) =>
       createPDF(
@@ -131,88 +138,88 @@ const createInsurance = async (userDetails) => {
   }
 };
 
-createInsurance([
-  {
-    name: "Ayman",
-    country: "Wonderland",
-    passportNumber: "123456789",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Babatunde",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Noorah",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Hesham",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Abdullah",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Lateef",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Umrah",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Hajj",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Bab Umrah",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-  {
-    name: "Happiness",
-    country: "Builderland",
-    passportNumber: "987654321",
-    arabicName: "أيمن",
-    arabicNationality: "مصري",
-    policyNumber: "123456789",
-  },
-]);
+// createInsurance([
+//   {
+//     name: "Ayman",
+//     country: "Wonderland",
+//     passportNumber: "123456789",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Babatunde",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Noorah",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Hesham",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Abdullah",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Lateef",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Umrah",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Hajj",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Bab Umrah",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+//   {
+//     name: "Happiness",
+//     country: "Builderland",
+//     passportNumber: "987654321",
+//     arabicName: "أيمن",
+//     arabicNationality: "مصري",
+//     policyNumber: "123456789",
+//   },
+// ]);
 
 module.exports = {
   createInsurance,
