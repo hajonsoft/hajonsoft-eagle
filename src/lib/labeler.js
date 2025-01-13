daaa5b3b924888dba2bde7f4a6a024faaac3d12a
Hesham Jones <30395704+abddeen@users.noreply.github.com>
2025-01-13 22:34:47 +1300

    fix: commonjs instead of es6 (#248)
    * fix: commonjs instead of es6
    
    * fix: update firebase dep
    
    * lock
    
    ---------
    
    Co-authored-by: AymanA <aliayman@alumni.harvard.edu>

--- src/lib/labeler.js
+++ src/lib/labeler.js
@@ -1,20 +1,23 @@
-export async function labeler(page, additionalValue, selector) {
-    // Get the innerText of the specified selector
-    const originalText = await page.$eval(selector, (el) => el.innerText);
-  
-    // Combine the original text with the additional value
-    const updatedText = `${originalText} ${additionalValue}`;
-  
-    // Update the innerText of the specified selector
-    await page.$eval(
-      selector,
-      (el, newText) => {
-        el.innerText = newText;
-      },
-      updatedText
-    );
-  
-    // Return the updated text for reference
-    return updatedText;
-  }
-  
+async function labeler(page, additionalValue, selector) {
+  // Get the innerText of the specified selector
+  const originalText = await page.$eval(selector, (el) => el.innerText);
+
+  // Combine the original text with the additional value
+  const updatedText = `${originalText} ${additionalValue}`;
+
+  // Update the innerText of the specified selector
+  await page.$eval(
+    selector,
+    (el, newText) => {
+      el.innerText = newText;
+    },
+    updatedText
+  );
+
+  // Return the updated text for reference
+  return updatedText;
+}
+
+module.exports = {
+  labeler,
+};
